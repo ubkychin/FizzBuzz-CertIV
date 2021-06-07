699be01bff8f3fae3bafa128c12f5b3f0784879f
Anh Nguyen <anguyen@swin.edu.au>
2021-06-08 09:56:30 +1000

    Fixed up formatting errors

--- Program.cs
+++ Program.cs
@@ -1,30 +1,21 @@
 ﻿using System;
 
-namespace FizzBuzz
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-        for (int counter = 1;
-        counter <= 100; counter++)
+namespace FizzBuzz {
+    class Program {
+        static void Main (string[] args) {
+            for (int counter = 1; counter <= 100; counter++)
 
-        if (counter % 3 == 0 && counter % 5 == 0) {
+            if (counter % 3 == 0 && counter % 5 == 0) {
 
-        System.Console.WriteLine("FizzBuzz");
+                System.Console.WriteLine ("FizzBuzz");
+            }
+            else if (counter % 3 == 0) {
+                System.Console.WriteLine ("Fizz");
+            } else if (counter % 5 == 0) {
+                System.Console.WriteLine ("Buzz");
+            } else {
+                System.Console.WriteLine (counter);
+            }
         }
-
-        else if (counter % 3 == 0) {
-        System.Console.WriteLine("Fizz");
-        }
-        
-        else if (counter % 5 == 0) {
-        System.Console.WriteLine("Buzz");
-       }
-
-        else {
-        System.Console.WriteLine(counter);
-        }
-       }
     }
 }
